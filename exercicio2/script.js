2ae39a565aa4fdc7912b2046a50533ba559ac106
unknown <iagosto.nasc@gmail.com>
2022-08-31 16:23:49 -0300

    commit A009-String

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStringTrim = minhaString.trim()
+
+console.log(minhaString);
+console.log(minhaString.length);
+
+console.log(minhaStringTrim);
+console.log(minhaStringTrim.length);
+
+const minhaStringNova = minhaString.replaceAll("________", "sticioso")
+console.log(minhaStringNova);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-//Crie a const para a frase aqui
+const frase = (`Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`)
+console.log(frase);
+
+const fraseNova = (frase.replaceAll ("verde", "rosa"))
+const fraseNova2 = (fraseNova.replaceAll("azul", "laranja"))
+console.log(fraseNova2);
+
+console.log(fraseNova2.includes("verde"));
+console.log(fraseNova2.includes("laranja"));
+console.log(fraseNova2.toUpperCase());
+
+
+
+
+
